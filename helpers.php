54f014921c7a186028620d14eb428df8021a06fc
behnamhamidi1991 <eslp91@hotmail.com>
2024-10-05 16:19:47 +0330

    Router

--- helpers.php
+++ helpers.php
@@ -11,6 +11,7 @@ function basePath($path = '')
     return __DIR__ . '/' . $path;
 }
 
+
 /**
  * Load a view
  * 
@@ -26,7 +27,6 @@ function loadView($name) {
     } else {
         echo "View '{$name}' not found!";
     }
-
 }
 
 /**
@@ -43,4 +43,28 @@ function loadPartial($name) {
     } else {
         echo "Partial '{$name}' not found!";
     }
+}
+
+/**
+ * Inspect (a) value(s)
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspect ($value) {
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+}
+
+/**
+ * Inspect (a) value(s) and die
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspectAndDie($value) {
+    echo '<pre>';
+    die(var_dump($value));
+    echo '</pre>';
 }
