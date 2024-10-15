840157946ccf33fe4c8844d12324239fa3ce5be1
behnamhamidi1991 <eslp91@hotmail.com>
2024-10-15 10:20:28 +0330

    new files added

--- routes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    '/' => 'controllers/home.php',
-    '/listings' => 'controllers/listings/index.php',
-    '/listings/create' => 'controllers/listings/create.php',
-    '404' => 'controllers/error/404.php'
-];
-
