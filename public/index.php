2d776223b099fdfa1ac6e8b9eebf9805fae71a5f
= <eslp91@hotmail.com>
2024-10-18 21:29:58 +0330

    Update the code

--- public/index.php
+++ public/index.php
@@ -7,12 +7,16 @@ $config = require basePath('config/db.php');
 
 $db = new Database($config);
 
+// Instantiate a new router
 $router = new Router();
 
+// Get routes
 $routes = require basePath('routes.php');
 
+// Get current URI and HTTP method
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
+// Route the request
 $router->route($uri, $method);
 
