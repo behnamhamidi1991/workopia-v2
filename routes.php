08c0570ed386063ca259d0784ae5887314c74ddc
= <eslp91@hotmail.com>
2024-10-22 00:24:11 +0330

    main

--- routes.php
+++ routes.php
@@ -1,16 +1,6 @@
 <?php 
 
-// return [
-//     '/' => 'controllers/home.php',
-//     '/listings' => 'controllers/listings/index.php',
-//     '/listings/create' => 'controllers/listings/create.php',
-// ];
-
 $router->get('/', 'HomeController@index');
-
-
-
-// $router->get('/', 'controllers/home.php');
-// $router->get('/listings', 'controllers/listings/index.php');
-// $router->get('/listings/create', 'controllers/listings/create.php');
-// $router->get('/listing', 'controllers/listings/show.php');
+$router->get('/listings', 'ListingController@index');
+$router->get('/listings/create', 'ListingController@create');
+$router->get('/listing', 'ListingController@show');
