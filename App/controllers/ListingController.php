0551eed05895e3b01a914ae63f0281ce055730ef
behnamhamidi1991 <eslp91@hotmail.com>
2024-10-22 11:37:11 +0330

    Validation and required fields

--- App/controllers/ListingController.php
+++ App/controllers/ListingController.php
@@ -22,8 +22,6 @@ class ListingController {
      */
     public function index() 
     { 
-        inspectAndDie(Validation::match('test', 'test'));
-
         $listings = $this->db->query('SELECT * FROM listings')->fetchAll();
 
         loadView('listings/index', [
@@ -58,4 +56,32 @@ class ListingController {
             'listing' => $listing
         ]);
     }
+
+    /**
+     * Store data in database
+     * 
+     * @return void
+     */
+    public function store() {
+        $allowedFields = ['title', 'description', 'salary', 'tags', 'company', 'address', 'city', 'state', 'phone', 'email', 'requirements', 'benefits'];
+
+        $newListingData = array_intersect_key($_POST, array_flip($allowedFields));
+
+        $newListingData['user_id'] = 1;
+
+        $newListingData = array_map('sanitize', $newListingData);
+
+        $requiredFields = ['title', 'description', 'salary', 'email', 'city', 'state']; 
+
+        $errors = [];
+
+        foreach($requiredFields as $field) {
+            if(empty($newListingData[$field]) || !Validation::string($newListingData[$field])) {
+                $errors[$field] = ucfirst($field) . ' is required';
+            }
+        }
+
+        inspectAndDie($errors);
+    }
+
 }
