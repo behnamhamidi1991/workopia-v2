022787a354a329b91ae165665699b57aafba3425
= <eslp91@hotmail.com>
2024-10-24 00:56:07 +0330

    create user

--- App/views/users/create.view.php
+++ App/views/users/create.view.php
@@ -18,6 +18,7 @@
               name="name"
               placeholder="Full Name"
               class="w-full px-4 py-2 border rounded focus:outline-none"
+              value="<?= $user['name'] ?? '' ?>"
             />
           </div>
           <div class="mb-4">
@@ -26,6 +27,7 @@
               name="email"
               placeholder="Email Address"
               class="w-full px-4 py-2 border rounded focus:outline-none"
+              value="<?= $user['email'] ?? '' ?>"
             />
           </div>
            <div class="mb-4">
@@ -34,6 +36,7 @@
               name="city"
               placeholder="City"
               class="w-full px-4 py-2 border rounded focus:outline-none"
+              value="<?= $user['city'] ?? '' ?>"
             />
           </div>
            <div class="mb-4">
@@ -42,6 +45,7 @@
               name="state"
               placeholder="State"
               class="w-full px-4 py-2 border rounded focus:outline-none"
+              value="<?= $user['state'] ?? '' ?>"
             />
           </div>
           <div class="mb-4">
